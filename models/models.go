2ed7a6f4ebdbcd7c3740ebe9bbde8f741b01467b
chaowat-lee13 <chaowat.lee@gmail.com>
2022-10-10 21:05:56 +0700

    add db connection and functionalities

--- models/models.go
+++ models/models.go
@@ -1,7 +1,23 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
+// Models is the wrapper for database
+type Models struct {
+	DB DBModel
+}
+
+// NewModels returns models with db pool
+func NewModels(db *sql.DB) Models {
+	return Models{
+		DB: DBModel{DB: db},
+	}
+}
+
+// Movie is the type for movies
 type Movie struct {
 	ID          int          `json:"id"`
 	Title       string       `json:"title"`
@@ -10,12 +26,13 @@ type Movie struct {
 	ReleaseDate time.Time    `json:"release_date"`
 	Runtime     int          `json:"runtime"`
 	Rating      int          `json:"rating"`
-	MPAARating  string       `json:"mapp_rating"`
+	MPAARating  string       `json:"mpaa_rating"`
 	CreatedAt   time.Time    `json:"created_at"`
 	UpdatedAt   time.Time    `json:"updated_at"`
 	MovieGenre  []MovieGenre `json:"-"`
 }
 
+// Genre is the type for genre
 type Genre struct {
 	ID        int       `json:"id"`
 	GenreName string    `json:"genre_name"`
@@ -23,6 +40,7 @@ type Genre struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
+// MovieGenre is the type for movie genre
 type MovieGenre struct {
 	ID        int       `json:"id"`
 	MovieID   int       `json:"movie_id"`
